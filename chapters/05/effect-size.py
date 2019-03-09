fcdaa2530e28f860fd8ee213875a2a5c4e8c7968
Jerome White <jerome@thewex.net>
2019-03-09 08:12:19 +0530

    Move variance specialised system to global module

--- chapters/05/effect-size.py
+++ chapters/05/effect-size.py
@@ -1,44 +1,11 @@
 import sys
 import csv
 import math
-import operator as op
-import itertools as it
-import collections as cl
 
-import numpy as np
+from irstats.systems import VarianceSystems
 
-from irstats.systems import Systems
-
-class ESSystems(Systems):
-    def __init__(self, fp):
-        super().__init__(fp)
-
-        axes = it.chain(range(2), (None, ))
-        mean = lambda x: self.data.mean(axis=x)
-        (self.s_mean, self.t_mean, self.m_mean) = map(mean, axes)
-
-    #
-    # Equation 5.16
-    #
-    def variance(self):
-        #
-        # S_E2
-        #
-        s = 0
-        for (i, m) in enumerate(self.s_mean):
-            x = self.m_mean - m
-            for (j, n) in enumerate(self.t_mean):
-                s += (self.data[(j, i)] - n + x) ** 2
-
-        #
-        # phi_E2
-        #
-        p = op.mul(*[ x - 1 for x in self.data.shape ])
-
-        return s / p
-
-systems = ESSystems(sys.stdin)
-deviation = math.sqrt(systems.variance())
+systems = VarianceSystems(sys.stdin)
+deviation = math.sqrt(systems.V())
 
 fieldnames = [
     'system_1',
--- irstats/systems.py
+++ irstats/systems.py
@@ -1,3 +1,4 @@
+import operator as op
 import itertools as it
 
 import numpy as np
@@ -19,3 +20,27 @@ class Systems:
     def differences(self):
         for i in self.pairs():
             yield (i, np.subtract(*[ self[x].mean() for x in i ]))
+
+class VarianceSystems(Systems):
+    def __init__(self, fp):
+        super().__init__(fp)
+
+        axes = it.chain(range(2), (None, ))
+        mean = lambda x: self.data.mean(axis=x)
+        (self.s_mean, self.t_mean, self.m_mean) = map(mean, axes)
+
+    def S(self):
+        s = 0
+
+        for (i, m) in enumerate(self.s_mean):
+            x = self.m_mean - m
+            for (j, n) in enumerate(self.t_mean):
+                s += (self.data[(j, i)] - n + x) ** 2
+
+        return s
+
+    def phi(self):
+        return op.mul(*[ x - 1 for x in self.data.shape ])
+
+    def V(self):
+        return self.S() / self.phi()
