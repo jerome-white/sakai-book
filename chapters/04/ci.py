b424588889953e0e7c34b6035459b44c4bc902d8
Jerome White <jerome@thewex.net>
2019-03-10 20:00:39 +0530

    Attempt at adding t_inv calculation

--- chapters/04/ci.py
+++ chapters/04/ci.py
@@ -4,13 +4,27 @@ import math
 import operator as op
 import itertools as it
 import collections as cl
+from argparse import ArgumentParser
 
 import numpy as np
+from scipy.stats import t
 
 from irstats.systems import VarianceSystems
 
+arguments = ArgumentParser()
+arguments.add_argument('--confidence', type=float, default=0.95)
+args = arguments.parse_args()
+
+assert(0 <= args.confidence <= 1)
+
 systems = VarianceSystems(sys.stdin)
-MOE = math.sqrt(systems.V() / systems.topics)
+
+#
+# EXCEL::T.INV.2T(p, df) == t.ppf((1 + (1 - p)) / 2, df)
+#
+t_inv = t.ppf((1 + args.confidence) / 2, systems.topics)
+
+MOE = t_inv * math.sqrt(systems.V() / systems.topics)
 
 fieldnames = [
     'system_1',
