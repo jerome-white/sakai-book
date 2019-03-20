744c283149cc5c1ca35a5aa3dcb2fdba488c244c
Jerome White <jerome@thewex.net>
2019-03-20 19:06:24 +0530

    Broader definition of "Systems"
    The Systems class works well for paired data of equal sizes, but doing
    more advanced manipulation may be complicated. This class serves as a
    more programmatic interface for that more general concept of what a
    set of IR results can be.

---
+++ irstats/results.py
@@ -0,0 +1,69 @@
+import itertools as it
+from pathlib import Path
+
+import pandas as pd
+
+__all__ = [
+    'Results',
+]
+
+Evaluation = cl.namedtuple('Evaluation', 'system, topic, metric')
+PairVal = cl.namedtuple('PairVal', 'system_1, system_2, value')
+
+class Results:
+    def __init__(self, data, metric):
+        self.e = Evaluation('system', 'topic', metric)
+
+        try:
+            fp = open(data)
+        except TypeError:
+            fp = data
+
+        self.df = pd.read_csv(fp, usecols=self.e)
+
+        if fp is not None:
+            fp.close()
+
+    def _is(self, grouper, mark, check):
+        baseline = None
+
+        for (_, g) in self.df.groupby(grouper):
+            ptr = mark(g)
+            if baseline is None:
+                baseline = ptr
+            elif check(baseline, ptr):
+                return False
+
+        return True
+
+    def ispaired(self):
+        return self._is(self.e.topic,
+                        lambda x: set(x[self.e.system]),
+                        lambda x, y: x.symmetric_difference(y))
+
+    def isegs(self):
+        return self._is(self.e.system,
+                        lambda x: len(x),
+                        lambda x, y: x != y)
+
+    def pairs(self):
+        systems = self.df[self.e.system].unique()
+        yield from it.combinations(systems, r=2)
+
+    def differences(self):
+        items = (self
+                 .astable()
+                 .mean()
+                 .iteritems())
+
+        for ((i, x), (j, y)) in it.combinations(items, r=2):
+            yield PairVal(i, j, x - y)
+
+    def astable(self):
+        return (self.df.pivot(index=self.e.topic,
+                              columns=self.e.system,
+                              values=self.e.metric))
+
+    def sizes(self):
+        for (i, g) in self.df.groupby(self.e.system):
+            yield (i, len(g))
