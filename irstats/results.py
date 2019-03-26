7876be33baade1cff82343cc3d5d91caf2e6b2d8
Jerome White <jerome@thewex.net>
2019-03-26 09:20:51 +0530

    Pivot results be default
    Instead of tidy format, use a more "systems"-like format (systems are
    columns, runs are rows). This simplifies much of the processing the
    class has to do.

--- irstats/results.py
+++ irstats/results.py
@@ -1,3 +1,4 @@
+import csv
 import itertools as it
 import collections as cl
 from pathlib import Path
@@ -13,59 +14,33 @@ Score = cl.namedtuple('Score', 'system, topic, score')
 PairVal = cl.namedtuple('PairVal', 'system_1, system_2, value')
 
 class Results:
-    def __init__(self, data, metric):
-        self.e = Evaluation('system', 'topic', metric)
-
-        try:
-            fp = open(data)
-        except TypeError:
-            fp = data
-
-        self.df = pd.read_csv(fp, usecols=self.e)
-
-        if fp is not None:
-            fp.close()
-
-    def _is(self, grouper, mark, check):
-        baseline = None
-
-        for (_, g) in self.df.groupby(grouper):
-            ptr = mark(g)
-            if baseline is None:
-                baseline = ptr
-            elif check(baseline, ptr):
-                return False
-
-        return True
+    def __init__(self, scores):
+        self.df = (pd
+                   .DataFrame
+                   .from_records(scores)
+                   .pivot(index='topic', columns='system', values='score'))
 
     def ispaired(self):
-        return self._is(self.e.topic,
-                        lambda x: set(x[self.e.system]),
-                        lambda x, y: x.symmetric_difference(y))
+        return self.df.isna().sum().any()
 
-    def isegs(self):
-        return self._is(self.e.system,
-                        lambda x: len(x),
-                        lambda x, y: x != y)
+    def isgroupeq(self):
+        return len(self.df.count().unique()) < 2
 
     def pairs(self):
-        systems = self.df[self.e.system].unique()
-        yield from it.combinations(systems, r=2)
+        yield from it.combinations(self.df.columns, r=2)
 
     def differences(self):
-        items = (self
-                 .astable()
-                 .mean()
-                 .iteritems())
-
+        items = self.df.mean().iteritems()
         for ((i, x), (j, y)) in it.combinations(items, r=2):
             yield PairVal(i, j, x - y)
 
-    def astable(self):
-        return (self.df.pivot(index=self.e.topic,
-                              columns=self.e.system,
-                              values=self.e.metric))
-
     def sizes(self):
-        for (i, g) in self.df.groupby(self.e.system):
-            yield (i, len(g))
+        yield from self.df.count().items()
+
+    @classmethod
+    def from_csv(cls, data):
+        with data.open() as fp:
+            scores = csv.DictReader(fp)
+            assert(all([ x in scores.fieldnames for x in Score._fields ]))
+
+            return cls(scores)
