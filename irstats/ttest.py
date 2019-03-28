183b6085543d317d0337cda409677cbb82c51397
Jerome White <jerome@thewex.net>
2019-03-28 09:41:04 +0530

    Begin to make __iter__ a template

--- irstats/ttest.py
+++ irstats/ttest.py
@@ -33,6 +33,10 @@ class T:
             'right_ci',
         ]
 
+    def __iter__(self):
+        for i in self.results.systems():
+            yield dict(zip(self.fieldnames, self.test(i)))
+
 class Paired(T):
     def __init__(self, results, alpha):
         assert(results.ispaired())
@@ -42,32 +46,30 @@ class Paired(T):
             'difference',
         ])
 
-    def __iter__(self):
-        for i in self.results.systems():
-            logging.debug(st.ttest_rel(*i.values()))
+    def test(self, i):
+        logging.debug(st.ttest_rel(*i.values()))
 
-            dj = np.subtract(*i.values())
-            n = len(dj)
-            df = n - 1
+        dj = np.subtract(*i.values())
+        n = len(dj)
+        df = n - 1
 
-            # Equations 2.4, 2.5, and 2.6, respectively
-            dbar = np.mean(dj)
-            Vd = sum(np.square(dj - dbar)) / df
-            variance = math.sqrt(Vd / n)
-            t0 = dbar / variance
+        # Equations 2.4, 2.5, and 2.6, respectively
+        dbar = np.mean(dj)
+        Vd = sum(np.square(dj - dbar)) / df
+        variance = math.sqrt(Vd / n)
+        t0 = dbar / variance
 
-            difference = op.sub(*map(np.mean, i.values()))
+        difference = op.sub(*map(np.mean, i.values()))
 
-            t0_ = abs(t0)
-            inverse = irs.t_inv(df, self.alpha)
-            reject = int(t0_ >= inverse)
-            p = st.t.sf(t0_, df) * 2
+        t0_ = abs(t0)
+        inverse = irs.t_inv(df, self.alpha)
+        reject = int(t0_ >= inverse)
+        p = st.t.sf(t0_, df) * 2
 
-            moe = inverse * variance
-            ci = [ f(dbar, moe) for f in (op.sub, op.add) ]
+        moe = inverse * variance
+        ci = [ f(dbar, moe) for f in (op.sub, op.add) ]
 
-            output = (*i.keys(), t0, df, p, reject, *ci, difference)
-            yield dict(zip(self.fieldnames, output))
+        return (*i.keys(), t0, df, p, reject, *ci, difference)
 
 class Unpaired(T):
     def __init__(self, results, alpha):
@@ -80,63 +82,59 @@ class Unpaired(T):
         ])
 
 class Student(Unpaired):
-    def __iter__(self):
-        for i in self.results.systems():
-            logging.debug(st.ttest_ind(*i.values(), equal_var=True))
+    def test(self, i):
+        logging.debug(st.ttest_ind(*i.values(), equal_var=True))
 
-            x = i.values()
-            (n1, n2) = map(len, x)
-            xbar = [ np.mean(y) for y in x ]
-            S = [ sum(np.square(np.subtract(*y))) for y in zip(x, xbar) ]
+        x = i.values()
+        (n1, n2) = map(len, x)
+        xbar = [ np.mean(y) for y in x ]
+        S = [ sum(np.square(np.subtract(*y))) for y in zip(x, xbar) ]
 
-            df = n1 + n2 - 2
+        df = n1 + n2 - 2
 
-            Vp = sum(S) / df
-            variance = math.sqrt(Vp * (1 / n1 + 1 / n2))
-            t0 = op.sub(*xbar) / variance
+        Vp = sum(S) / df
+        variance = math.sqrt(Vp * (1 / n1 + 1 / n2))
+        t0 = op.sub(*xbar) / variance
 
-            t0_ = abs(t0)
-            inverse = irs.t_inv(df, self.alpha)
-            reject = int(t0_ >= inverse)
-            p = st.t.sf(t0_, df) * 2
+        t0_ = abs(t0)
+        inverse = irs.t_inv(df, self.alpha)
+        reject = int(t0_ >= inverse)
+        p = st.t.sf(t0_, df) * 2
 
-            moe = inverse * variance
-            difference = op.sub(*xbar)
-            ci = [ f(difference, moe) for f in (op.sub, op.add) ]
+        moe = inverse * variance
+        difference = op.sub(*xbar)
+        ci = [ f(difference, moe) for f in (op.sub, op.add) ]
 
-            output = (*i.keys(), t0, df, p, reject, *ci, *xbar)
-            yield dict(zip(self.fieldnames, output))
+        return (*i.keys(), t0, df, p, reject, *ci, *xbar)
 
 class Welch(Unpaired):
-    def __iter__(self):
-        for i in self.results.systems():
-            logging.debug(st.ttest_ind(*i.values(), equal_var=False))
+    def test(self, i):
+        logging.debug(st.ttest_ind(*i.values(), equal_var=False))
 
-            x = i.values()
-            n = list(map(len, x))
-            xbar = [ np.mean(j) for j in x ]
+        x = i.values()
+        n = list(map(len, x))
+        xbar = [ np.mean(j) for j in x ]
 
-            V = []
-            for (j, k, l) in zip(x, xbar, n):
-                value = sum(np.square(np.subtract(j, k))) / (l - 1)
-                V.append(value)
+        V = []
+        for (j, k, l) in zip(x, xbar, n):
+            value = sum(np.square(np.subtract(j, k))) / (l - 1)
+            V.append(value)
 
-            V_n = sum(it.starmap(op.truediv, zip(V, n)))
-            variance = math.sqrt(V_n)
+        V_n = sum(it.starmap(op.truediv, zip(V, n)))
+        variance = math.sqrt(V_n)
 
-            df = V_n ** 2
-            df /= sum([ (j / k) ** 2 / (k - 1) for (j, k) in zip(V, n) ])
+        df = V_n ** 2
+        df /= sum([ (j / k) ** 2 / (k - 1) for (j, k) in zip(V, n) ])
 
-            tw0 = op.sub(*xbar) / variance
+        tw0 = op.sub(*xbar) / variance
 
-            tw0_ = abs(tw0)
-            inverse = irs.t_inv(df, self.alpha)
-            reject = int(tw0_ >= inverse)
-            p = st.t.sf(tw0_, df) * 2
+        tw0_ = abs(tw0)
+        inverse = irs.t_inv(df, self.alpha)
+        reject = int(tw0_ >= inverse)
+        p = st.t.sf(tw0_, df) * 2
 
-            moe = inverse * variance
-            difference = op.sub(*xbar)
-            ci = [ f(difference, moe) for f in (op.sub, op.add) ]
+        moe = inverse * variance
+        difference = op.sub(*xbar)
+        ci = [ f(difference, moe) for f in (op.sub, op.add) ]
 
-            output = (*i.keys(), tw0, df, p, reject, *ci, *xbar)
-            yield dict(zip(self.fieldnames, output))
+        return (*i.keys(), tw0, df, p, reject, *ci, *xbar)
