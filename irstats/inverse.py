68390756af2508aac1e582df17998713e48c96d2
Jerome White <jerome@thewex.net>
2019-03-28 08:55:35 +0530

    Moving away from object-based inverse calculation
    Inverses will be functions. For now that only includes Student's
    t-distribution.

--- irstats/inverse.py
+++ irstats/inverse.py
@@ -1,45 +1,8 @@
-from scipy import stats
-from statsmodels.stats import libqsturng
+import scipy.stats as st
 
 __all__ = [
-    'Inverse',
-    'TInverse',
-    'QInverse',
+    't_inv',
 ]
 
-class Inverse:
-    def __init__(self, alpha, df):
-        self.alpha = alpha
-        self.df = df
-
-    def __float__(self):
-        return float(self.inv())
-
-    def inv(self):
-        raise NotImplementedError()
-
-#
-# https://support.office.com/en-us/article/t-inv-2t-function-ce72ea19-ec6c-4be7-bed2-b9baf2264f17
-# EXCEL::T.INV.2T(p, df) == t.ppf(1 - (p / 2), df)
-#
-class TInverse(Inverse):
-    def __init__(self, alpha, df, tail=True):
-        super().__init__(alpha, df)
-
-        self.p = 1 - self.alpha
-        if tail:
-            self.p /= 2
-
-    def inv(self):
-        return stats.t.ppf(1 - self.p, self.df)
-
-#
-# https://stat.ethz.ch/R-manual/R-patched/library/stats/html/Tukey.html
-#
-class QInverse(Inverse):
-    def __init__(self, alpha, df, systems):
-        super().__init__(alpha, df)
-        self.systems = systems
-
-    def inv(self):
-        return libqsturng.qsturng(self.alpha, self.systems, self.df)
+def t_inv(phi, P):
+    return st.t.ppf(1 - (1 - P) / 2, phi)
