1de1d0b8691658d86090ddcd91f9d2b7bda85603
Jerome White <jerome@thewex.net>
2019-03-29 18:28:31 +0530

    Completed two-way ANOVA

--- chapters/03/two-way.py
+++ chapters/03/two-way.py
@@ -5,44 +5,88 @@ from argparse import ArgumentParser
 import irstats as irs
 # from irstats import Scores
 
-class TwoWay:
+class Anova:
     def __init__(self, scores, alpha):
         # assert(not scores.ispaired())
 
         self.scores = scores
         self.alpha = alpha
 
-        self.test = self.WR if self.scores.isreplicated() else self.WoR
+        self.m = self.scores.systems()
+        self.n = self.scores.topics()
 
-    def WoR(self):
+class WithoutReplication(Anova):
+    def test(self):
         s = self.scores()
-        m = len(s.columns)
 
         xbar = s.sum().sum() / s.count().sum()
         ST = s.apply(lambda x: (x - xbar) ** 2, axis='columns').sum().sum()
         SA = (s
               .apply(lambda x: len(x) * (x.mean() - xbar) ** 2, axis='rows')
               .sum())
+        SB = (s
+              .apply(lambda x: (x.mean() - xbar) ** 2, axis='columns')
+              .sum()) * self.m
+
         SE1 = ST - SA
-
-        SB = s.apply(lambda x: (x.mean() - xbar) ** 2, axis='columns').sum()
-        SB *= m
-
         SE2 = SE1 - SB
 
-        phiA = m - 1
-        phiB = len(s) - 1
-        phiE2 = (m - 1) * (n - 1)
+        phiA = self.m - 1
+        phiB = self.n - 1
+        phiE2 = phiA * phiB
 
         VA = SA / phiA
         VB = SB / phiB
         VE2 = SE2 / phiE2
 
-        F0 = VA / VE2
-        system_reject = int(F0 >= irs.F_inv(self.alpha, phiA, phiE2))
+        F0 = {}
+        for (i, j, k) in zip(('system', 'topic'), (VA, VB), (phiA, phiB)):
+            F = j / VE2
+            F0[i] = (F, int(F >= irs.F_inv(k, phiE2, self.alpha)))
 
-        F0_ = VB / VE2
-        topic_reject = int(F0 >= irs.F_inv(self.alpha, phiB, phiE2))
+class WithReplication(Anova):
+    def __init__(self, scores, alpha):
+        assert(scores.isreplicated())
+        super().__init__(scores, alpha)
+
+        r = self.scores.replicants()
+
+    def test(self):
+        s = self.scores()
+
+        xbar = s.mean().mean()
+        ST = s.apply(lambda x: (x - xbar) ** 2, axis='columns').sum().sum()
+        SA = (s
+              .apply(lambda x: (x.mean() - xbar) ** 2, axis='rows')
+              .sum()) * self.n * self.r
+        SB = (s
+              .apply(lambda x: (x.mean() - xbar) ** 2, axis='columns')
+              .sum()) * self.m * self.r
+
+        SE3 = (s
+               .reset_index()
+               .groupby('topic')
+               .apply(lambda x: (x + 1) ** 2)
+               .sum())
+
+        SAxB = ST - SA - SB - S3
+
+        phiA = self.m - 1
+        phiB = self.n - 1
+        phiAxB = phiA - phiB
+        phiE3 = self.m * self.n * (self.r - 1)
+
+        VA = SA / phiA
+        VB = SB / phiB
+        VAxB = SAxB / phiAxB
+        V3 = SE3 / phiE3
+
+        F0 = {}
+        for (i, j, k) in zip(('system-topic', 'system', 'topic'),
+                             (VAxB, VA, VB),
+                             (phiAxB, phiA, phiB)):
+            F = j / VE3
+            F0[i] = (F, int(F >= irs.F_inv(k, phiE3, self.alpha)))
 
 arguments = ArgumentParser()
 arguments.add_argument('--alpha', type=float, default=0.95)
