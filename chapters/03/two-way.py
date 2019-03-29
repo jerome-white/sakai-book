e2649a01868fcfe73515c851c342ca6030429c46
Jerome White <jerome@thewex.net>
2019-03-29 09:48:40 +0530

    Rough implementation of one-way ANOVA without replication

--- chapters/03/two-way.py
+++ chapters/03/two-way.py
@@ -8,15 +8,16 @@ import irstats as irs
 class TwoWay:
     def __init__(self, scores, alpha):
         # assert(not scores.ispaired())
-        
+
         self.scores = scores
         self.alpha = alpha
 
         self.test = self.WR if self.scores.isreplicated() else self.WoR
 
-    def test(self):
+    def WoR(self):
         s = self.scores()
-        
+        m = len(s.columns)
+
         xbar = s.sum().sum() / s.count().sum()
         ST = s.apply(lambda x: (x - xbar) ** 2, axis='columns').sum().sum()
         SA = (s
@@ -24,13 +25,24 @@ class TwoWay:
               .sum())
         SE1 = ST - SA
 
-        m = len(s.columns)
+        SB = s.apply(lambda x: (x.mean() - xbar) ** 2, axis='columns').sum()
+        SB *= m
+
+        SE2 = SE1 - SB
+
         phiA = m - 1
-        phiE1 = s.apply(lambda x: len(x) - m, axis='rows').sum()
+        phiB = len(s) - 1
+        phiE2 = (m - 1) * (n - 1)
 
-        F0 = (SA / phiA) / (SE1 / phiE1)
+        VA = SA / phiA
+        VB = SB / phiB
+        VE2 = SE2 / phiE2
 
-        reject = int(F0 >= irs.F_inv(self.alpha, phiA, phiE1))
+        F0 = VA / VE2
+        system_reject = int(F0 >= irs.F_inv(self.alpha, phiA, phiE2))
+
+        F0_ = VB / VE2
+        topic_reject = int(F0 >= irs.F_inv(self.alpha, phiB, phiE2))
 
 arguments = ArgumentParser()
 arguments.add_argument('--alpha', type=float, default=0.95)
