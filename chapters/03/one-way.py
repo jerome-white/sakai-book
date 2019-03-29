5bb602f894b03a5e0b926b478bd9672acbb3f1ec
Jerome White <jerome@thewex.net>
2019-03-29 09:08:50 +0530

    Start of ANOVA implementations

---
+++ chapters/03/one-way.py
@@ -0,0 +1,44 @@
+import sys
+import csv
+from argparse import ArgumentParser
+
+import irstats as irs
+# from irstats import Scores
+
+class OneWay:
+    def __init__(self, scores, alpha):
+        # assert(not scores.ispaired())
+        
+        self.scores = scores
+        self.alpha = alpha
+
+    def test(self):
+        s = self.scores()
+        
+        xbar = s.sum().sum() / s.count().sum()
+        ST = s.apply(lambda x: (x - xbar) ** 2, axis='columns').sum().sum()
+        SA = (s
+              .apply(lambda x: len(x) * (x.mean() - xbar) ** 2, axis='rows')
+              .sum())
+        SE1 = ST - SA
+
+        m = len(s.columns)
+        phiA = m - 1
+        phiE1 = s.apply(lambda x: len(x) - m, axis='rows').sum()
+
+        F0 = (SA / phiA) / (SE1 / phiE1)
+
+        reject = int(F0 >= irs.F_inv(self.alpha, phiA, phiE1))
+
+arguments = ArgumentParser()
+arguments.add_argument('--alpha', type=float, default=0.95)
+args = arguments.parse_args()
+
+assert(0 <= args.alpha <= 1)
+
+results = Scores.from_csv(sys.stdin)
+t = Paired(results, args.alpha)
+
+writer = csv.DictWriter(sys.stdout, fieldnames=t.fieldnames)
+writer.writeheader()
+writer.writerows(t)
