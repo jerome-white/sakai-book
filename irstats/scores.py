f58a053dfff1617e997aaa285d46f3cbddd7c5c5
Jerome White <jerome@thewex.net>
2019-04-08 10:33:02 +0530

    Use tidy as default internal format

--- irstats/scores.py
+++ irstats/scores.py
@@ -5,51 +5,68 @@ import collections as cl
 import pandas as pd
 
 Score = cl.namedtuple('Score', 'system, topic, score')
+Shape = cl.namedtuple('Shape', Score._fields + ('replication', ))
 
 class Scores:
     def __init__(self, scores):
         self.df = (pd
                    .DataFrame
                    .from_records(scores)
-                   .pivot(index='topic', columns='system', values='score')
-                   .astype(float))
+                   .astype({ 'score': float }))
 
     def __call__(self):
         return self.df
 
+    #
+    #
+    #
+
+    def _size(self, factor):
+        return self.df.groupby(factor, sort=False).apply(len).unique()
+
+    def astable(self):
+        return self.df.pivot(index='topic', columns='system', values='score')
+
+    def combinations(self, n=2, aspect='system'):
+        view = self.df[aspect]
+        for i in it.combinations(view.unique(), r=n):
+            values = [ self.df[view == x]['score'].tolist() for x in i ]
+            yield dict(zip(i, values))
+
+    def shape(self):
+        keys = [ 'system', 'topic' ]
+        systop = [ self.df[x].unique().size for x in keys ]
+
+        sizes = self._size(keys)
+        r = sizes.item() if len(sizes) == 1 else float('nan')
+
+        return Shape(*systop, len(self.df), r)
+
+    #
+    # Data is "paired" if every system contains a score for every
+    # topic.
+    #
     def ispaired(self):
-        return not self.df.isna().sum().any()
+        topics = None
+
+        for (_, g) in self.df.groupby('system', sort=False):
+            t = sorted(g['topic'].tolist())
+            if topics is None:
+                topics = t
+            elif topics != t:
+                return False
+
+        return True
 
     def isgroupeq(self):
-        return len(self.df.count().unique()) < 2
+        return self._size('system').size == 1
 
-    def isreplicated(self):
-        return self.df.index.duplicated.any()
+    # def isreplicated(self):
+    #     return self.df.index.duplicated().any()
 
-    def combinations(self, n=2):
-        for i in it.combinations(self.df.columns, r=n):
-            j = [ self.df[x].tolist() for x in i ]
-            yield dict(zip(i, j))
-
-    def systems(self):
-        return len(self.df.columns)
-
-    def topics(self):
-        if not self.ispaired():
-            raise ValueError('Scores are not paired')
-
-        return len(self.df.index.unique())
-
-    def replicants(self):
-        if not self.ispaired():
-            raise ValueError('Scores are not paired')
-
-        r = self.df.index.value_counts().unique().tolist()
-        if len(r) > 1:
-            raise ValueError('Replications not equal')
-
-        return r.pop()
-
+    #
+    # Read data from a CSV stream
+    #
     @classmethod
     def from_csv(cls, data):
         try:
