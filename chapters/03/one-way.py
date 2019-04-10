e3e8f12587f4b628fa6c19d794e61971dcd71c84
Jerome White <jerome@thewex.net>
2019-04-10 09:02:48 +0530

    ANOVA runs entirely from module

--- chapters/03/one-way.py
+++ chapters/03/one-way.py
@@ -1,68 +1,13 @@
 import sys
 import csv
-import math
 import logging
-import operator as op
 from argparse import ArgumentParser
 
-import scipy.stats as st
-
-import irstats as irs
-# from irstats import Scores
+from irstats import Scores, OneWay
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S')
-
-class OneWay_:
-    def __init__(self, scores, alpha):
-        # assert(not scores.ispaired())
-
-        self.scores = scores
-        self.alpha = alpha
-
-        self.m = self.scores.systems()
-        self.n = self.scores.topics()
-
-    def test(self):
-        s = self.scores()
-
-        xbar = s.sum().sum() / s.count().sum()
-        ST = s.apply(lambda x: (x - xbar) ** 2, axis='columns').sum().sum()
-        SA = (s
-              .apply(lambda x: len(x) * (x.mean() - xbar) ** 2, axis='rows')
-              .sum())
-        SE1 = ST - SA
-
-        phiA = self.m - 1
-        phiE1 = s.count().sum() - self.m
-
-        F0 = (SA / phiA) / (SE1 / phiE1)
-        reject = int(F0 >= irs.F_inv(phiA, phiE1, self.alpha))
-        p = st.f.sf(F0, phiA, phiE1)
-
-        VE1 = SE1 / phiE1
-        MOE = irs.t_inv(phiE1, self.alpha) * math.sqrt(VE1 / self.n)
-        ci = []
-        for i in s:
-            ci.append([ f(s[i].mean(), MOE) for f in (op.sub, op.add) ])
-
-        logging.debug({
-            'm': self.m,
-            'n': self.n,
-            'phiA': phiA,
-            'phiE1': phiE1,
-        })
-        logging.info({
-            'ST': ST,
-            'SE1': SE1,
-            'SA': SA,
-            'VA': SA / phiA,
-            'VE1': VE1,
-            'F0': F0,
-            'reject': reject,
-            'p-value': p,
-        })
 
 arguments = ArgumentParser()
 arguments.add_argument('--alpha', type=float, default=0.95)
@@ -70,13 +15,15 @@ args = arguments.parse_args()
 
 assert(0 <= args.alpha <= 1)
 
-results = irs.Scores.from_csv(sys.stdin)
-# t = OneWay(results, args.alpha)
-# t.test()
+scores = Scores.from_csv(sys.stdin)
+anova = OneWay(scores, args.alpha)
 
 writer = None
-for i in irs.OneWay(results, args.alpha):
+for i in anova:
     if writer is None:
         writer = csv.DictWriter(sys.stdout, fieldnames=i._fields)
         writer.writeheader()
     writer.writerow(i._asdict())
+
+for (i, j) in anova.ci():
+    logging.info('{name} {ci}'.format(name=i, ci=list(j)))
--- chapters/03/two-way.py
+++ chapters/03/two-way.py
@@ -1,98 +1,13 @@
 import sys
 import csv
+import logging
 from argparse import ArgumentParser
 
-import irstats as irs
-# from irstats import Scores
+from irstats import Scores, TwoWay
 
-class Anova:
-    def __init__(self, scores, alpha):
-        # assert(not scores.ispaired())
-
-        self.scores = scores
-        self.alpha = alpha
-
-        self.m = self.scores.systems()
-        self.n = self.scores.topics()
-
-class WithoutReplication(Anova):
-    def test(self):
-        s = self.scores()
-
-        xbar = s.sum().sum() / s.count().sum()
-        ST = s.apply(lambda x: (x - xbar) ** 2, axis='columns').sum().sum()
-        SA = (s
-              .apply(lambda x: len(x) * (x.mean() - xbar) ** 2, axis='rows')
-              .sum())
-        SB = (s
-              .apply(lambda x: (x.mean() - xbar) ** 2, axis='columns')
-              .sum()) * self.m
-
-        SE1 = ST - SA
-        SE2 = SE1 - SB
-
-        phiA = self.m - 1
-        phiB = self.n - 1
-        phiE2 = phiA * phiB
-
-        VA = SA / phiA
-        VB = SB / phiB
-        VE2 = SE2 / phiE2
-
-        F0 = {}
-        for (i, j, k) in zip(('system', 'topic'), (VA, VB), (phiA, phiB)):
-            F = j / VE2
-            F0[i] = (F, int(F >= irs.F_inv(k, phiE2, self.alpha)))
-
-        VE2 = SE2 / phiE2
-        MOE = irs.t_inv(phiE2, self.alpha) * math.sqrt(VE2 / self.n)
-        ci = []
-        for i in s:
-            ci.append([ f(s[i].mean(), MOE) for f in (op.sub, op.add) ])
-
-class WithReplication(Anova):
-    def __init__(self, scores, alpha):
-        assert(scores.isreplicated())
-        super().__init__(scores, alpha)
-
-        r = self.scores.replicants()
-
-    def test(self):
-        s = self.scores()
-
-        xbar = s.mean().mean()
-        ST = s.apply(lambda x: (x - xbar) ** 2, axis='columns').sum().sum()
-        SA = (s
-              .apply(lambda x: (x.mean() - xbar) ** 2, axis='rows')
-              .sum()) * self.n * self.r
-        SB = (s
-              .apply(lambda x: (x.mean() - xbar) ** 2, axis='columns')
-              .sum()) * self.m * self.r
-
-        SE3 = (s
-               .reset_index()
-               .groupby('topic')
-               .apply(lambda x: (x + 1) ** 2)
-               .sum())
-
-        SAxB = ST - SA - SB - S3
-
-        phiA = self.m - 1
-        phiB = self.n - 1
-        phiAxB = phiA - phiB
-        phiE3 = self.m * self.n * (self.r - 1)
-
-        VA = SA / phiA
-        VB = SB / phiB
-        VAxB = SAxB / phiAxB
-        V3 = SE3 / phiE3
-
-        F0 = {}
-        for (i, j, k) in zip(('system-topic', 'system', 'topic'),
-                             (VAxB, VA, VB),
-                             (phiAxB, phiA, phiB)):
-            F = j / VE3
-            F0[i] = (F, int(F >= irs.F_inv(k, phiE3, self.alpha)))
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S')
 
 arguments = ArgumentParser()
 arguments.add_argument('--alpha', type=float, default=0.95)
@@ -100,9 +15,15 @@ args = arguments.parse_args()
 
 assert(0 <= args.alpha <= 1)
 
-results = Scores.from_csv(sys.stdin)
-t = Paired(results, args.alpha)
+scores = Scores.from_csv(sys.stdin)
+anova = TwoWay(scores, args.alpha)
 
-writer = csv.DictWriter(sys.stdout, fieldnames=t.fieldnames)
-writer.writeheader()
-writer.writerows(t)
+writer = None
+for i in anova:
+    if writer is None:
+        writer = csv.DictWriter(sys.stdout, fieldnames=i._fields)
+        writer.writeheader()
+    writer.writerow(i._asdict())
+
+for (i, j) in anova.ci():
+    logging.info('{name} {ci}'.format(name=i, ci=list(j)))
