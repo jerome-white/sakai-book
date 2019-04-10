8acccfa7d41db0307bd40e9e850153120aed1c9b
Jerome White <jerome@thewex.net>
2019-04-10 12:34:38 +0530

    (Re)Addition of studentised range distribution

--- irstats/inverse.py
+++ irstats/inverse.py
@@ -1,7 +1,11 @@
 import scipy.stats as st
+from statsmodels.stats import libqsturng
 
 def t_inv(phi, P):
     return st.t.ppf(1 - (1 - P) / 2, phi)
 
 def F_inv(phi1, phi2, P):
     return st.f.ppf(1 - P, phi1, phi2)
+
+def q_inv(m, phi, P):
+    return libqsturng.qsturng(P, m, phi)
