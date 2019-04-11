412ff803a6b19965ae3bcadc7ed3f469d5852660
Jerome White <jerome@thewex.net>
2019-04-11 14:17:39 +0530

    Maintain names for the left and right intervals

--- irstats/ci.py
+++ irstats/ci.py
@@ -6,12 +6,18 @@ class ConfidenceInterval:
         self.mean = mean
         self.MOE = MOE
 
-        self.f = (op.sub, op.add)
+        self.f = {
+            'left': op.sub,
+            'right': op.add,
+        }
         self.itr = None
 
     def __iter__(self):
-        self.itr = iter(self.f)
+        self.itr = iter(self.f.values())
         return self
 
     def __next__(self):
         return next(self.itr)(self.mean, self.MOE)
+
+    def asdict(self):
+        return dict(zip(self.f.keys(), self))
