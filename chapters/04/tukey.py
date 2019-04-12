e8a0b982e7fc10b49aeda845fc8219f6377a4963
Jerome White <jerome@thewex.net>
2019-04-12 12:15:21 +0530

    Addition of p-value and confidence interval

--- chapters/04/tukey.py
+++ chapters/04/tukey.py
@@ -1,34 +1,47 @@
 import sys
 import csv
 import math
+import logging
 import collections as cl
 from argparse import ArgumentParser
 
 import numpy as np
+import scipy.stats as st
 
 # from irstats import Scores, OneWay, TwoWay
 import irstats as irs
+from irstats.ci import ConfidenceInterval
 
-Result = cl.namedtuple('Result', 'system1, system2, difference, t, reject')
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S')
+
+Result = cl.namedtuple('Result', 'system1, system2, difference, p, reject')
 
 class Tukey:
-    def __init__(self, scores, alpha):
+    def __init__(self, scores, alpha, Anova):
         self.scores = scores
         self.alpha = alpha
-
-        Anova = irs.TwoWay if self.scores.ispaired() else irs.OneWay
         self.anova = Anova(self.scores, alpha)
 
     def __iter__(self):
+        V = float(self.anova.E)
         q = irs.q_inv(self.anova.m, self.anova.phiE, self.alpha) / math.sqrt(2)
 
         for i in self.scores.combinations():
-            diff = np.subtract(*map(np.mean, i.values()))
-            V = self.anova.phiE * sum([ 1 / len(x) for x in i.values() ])
-            t = diff / math.sqrt(V)
-            reject = int(abs(t) >= q)
+            (x1, x2) = i.values()
+            diff = np.subtract(*map(np.mean, (x1, x2)))
 
-            yield Result(*i.keys(), diff, t, reject)
+            normv = math.sqrt(V * sum([ 1 / len(x) for x in (x1, x2) ]))
+            t = abs(diff / normv)
+            reject = int(t >= q)
+            p = st.t.sf(t, self.anova.m) * 2 # ???
+
+            result = Result(*i.keys(), diff, p, reject)
+
+            ci = ConfidenceInterval(diff, q * normv)
+
+            yield { **result._asdict(), **ci.asdict() }
 
 arguments = ArgumentParser()
 arguments.add_argument('--alpha', type=float, default=0.95)
