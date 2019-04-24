8361a9cfa3b3fc0277072da600056fc20fd39997
Jerome White <jerome@thewex.net>
2019-04-24 20:13:16 +0530

    Make unbias logic an actual function (and not a lambda)
    Unbias as a lambda prevents effect size from being calculated
    concurrently (since lambda's cannot be pickled). Unbiasing is now a
    template within the Effect framework.

--- irstats/effect.py
+++ irstats/effect.py
@@ -9,7 +9,7 @@ class Effect:
         (self.s, self.n) = map(sum, [ map(x, self.x) for x in (self.S, len) ])
 
     def __float__(self):
-        return self.correction(self.difference / math.sqrt(self.V()))
+        return self.difference / math.sqrt(self.V())
 
     def S(self, values):
         return np.sum(np.square(np.subtract(values, np.mean(values))))
@@ -17,39 +17,10 @@ class Effect:
     def V(self):
         raise NotImplementedError()
 
-class Hedge(Effect):
-    def __init__(self, x1, x2, unbiased=False):
-        super().__init__(x1, x2)
-
-        # Equation 5.10
-        if unbiased:
-            self.correction = lambda x: (1 - 3 / (4 * self.n - 9)) * x
-
-    # Equation 5.6
-    def V(self):
-        return self.s / (self.n - 2)
-
 class Cohen(Effect):
     # Equation 5.8
     def V(self):
         return self.s / self.n
-
-class Glass(Effect):
-    def __init__(self, x1, x2, baseline=None):
-        super().__init__(x1, x2)
-
-        if baseline is None:
-            self.baseline = x2
-        else:
-            self.baseline = baseline
-
-            # Equation 5.12
-            n2 = len(self.baseline)
-            self.correction = lambda x: (1 - 3 / (4 * n2 - 5)) * x
-
-    # Equation 5.11
-    def V(self):
-        return self.S(self.baseline) / (len(self.baseline) - 1)
 
 class TukeyEffect(Effect):
     def __init__(self, x1, x2, anova):
@@ -58,3 +29,40 @@ class TukeyEffect(Effect):
 
     def V(self):
         return self.anova.phiE
+
+class UnbiasedEffect(Effect):
+    def __init__(self, x1, x2, desired=False):
+        super().__init__(x1, x2)
+        self.desired = desired
+
+    def __float__(self):
+        value = super().__float__()
+        if self.desired:
+            value *= self.unbias()
+
+        return value
+
+    def unbias(self):
+        raise NotImplementedError()
+
+class Hedge(UnbiasedEffect):
+    # Equation 5.6
+    def V(self):
+        return self.s / (self.n - 2)
+
+    # Equation 5.10
+    def unbias(self):
+        return 1 - 3 / (4 * self.n - 9)
+
+class Glass(UnbiasedEffect):
+    def __init__(self, x1, x2, baseline=None):
+        super().__init__(x1, x2, bool(baseline))
+        self.baseline = x2 if baseline is None else baseline
+
+    # Equation 5.11
+    def V(self):
+        return self.S(self.baseline) / (len(self.baseline) - 1)
+
+    # Equation 5.12
+    def unbias(self):
+        return 1 - 3 / (4 * len(self.baseline) - 5)
