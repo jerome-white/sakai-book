4317f26c98ecc7fdda23fd82239fd3d33b7e2fe0
Jerome White <jerome@thewex.net>
2019-04-24 18:50:30 +0000

    Invalid attribute

--- irstats/tukey.py
+++ irstats/tukey.py
@@ -111,7 +111,7 @@ class RandomisedTukey:
         return self
 
     def __next__(self):
-        if not self.c:
+        if not self.info:
             raise StopIteration()
 
         (k, info) = self.info.popitem()
