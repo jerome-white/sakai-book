a5c10127b013cb587822ba82f5b2f251718c5c7a
Jerome White <jerome@thewex.net>
2019-04-25 22:51:05 +0530

    Simplified logic of multiprocessing
    * Use processor pools instead of queues
    * Sample size is a class instead of a function

--- chapters/06/samplesizeTTEST2.py
+++ chapters/06/samplesizeTTEST2.py
@@ -9,27 +9,29 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S')
 
-def func(incoming, outgoing, args):
-    delta = args.min_diff / math.sqrt(args.sigma)
+class Sample:
+    def __init__(self, n, alpha, beta, delta):
+        self.n = n
+        self.beta = beta
 
-    while True:
-        n = incoming.get()
-        lamb = math.sqrt(n) * delta
-        phi = n - 1
-
-        logging.debug('{} lambda:{} phi:{}'.format(n, lamb, phi))
-
+        lamb = math.sqrt(self.n) * delta
+        phi = self.n - 1
         w = st.t.ppf(1 - args.alpha / 2, phi)
 
         normalization = math.sqrt(1 + w ** 2 / (2 * phi))
         f = lambda x: (x * (1 - 1 / (4 * phi)) - lamb) / normalization
-        (left, right) = [ st.norm.cdf(f(g(w))) for g in (op.neg, op.pos) ]
+        self.pr = [ st.norm.cdf(f(g(w))) for g in (op.neg, op.pos) ]
 
-        beta = left + 1 - right
+    def __float__(self):
+        (left, right) = self.pr
 
-        logging.debug('{} {}: {} {}'.format(n, w, left, right))
+        return float(left + 1 - right)
 
-        outgoing.put((n, beta))
+    def __int__(self):
+        return self.n
+
+    def __bool__(self):
+        return 1 - self.beta < float(self)
 
 arguments = ArgumentParser()
 arguments.add_argument('--alpha', type=float,
@@ -44,22 +46,19 @@ arguments.add_argument('--radius', type=int, default=0)
 arguments.add_argument('--workers', type=int, default=mp.cpu_count())
 args = arguments.parse_args()
 
-incoming = mp.Queue()
-outgoing = mp.Queue()
-with mp.Pool(args.workers, func, (outgoing, incoming, args)):
+with mp.Pool(args.workers) as pool:
     norminv = lambda x: st.norm.ppf(x)
     (zalpha, zbeta) = map(norminv, (1 - args.alpha / 2, args.beta))
 
     min_delta = args.min_diff / math.sqrt(args.sigma)
     n = ((zalpha - zbeta) / min_delta) ** 2 + zalpha ** 2 / 2
+
     logging.debug(n)
 
     recommended = math.ceil(n)
     start = max(0, recommended - args.radius)
     stop = recommended + args.radius + 1
-    for i in range(start, stop):
-        outgoing.put(i)
 
-    for _ in range(stop - start):
-        (n, beta) = incoming.get()
-        logging.info('{} {}'.format(n, 1 - args.beta < beta))
+    f = lambda x: (x, args.alpha, args.beta, min_delta)
+    for s in pool.starmap(Sample, map(f, range(start, stop))):
+        logging.info('{} {}'.format(int(s), bool(s)))
