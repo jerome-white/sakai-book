1fe6c737b1c90e0669e6d7bddb45904550fc1455
Jerome White <jerome@thewex.net>
2019-04-27 05:28:27 +0530

    Use serial iteration
    Using parallel iteration for CI calculation opens the possibility for
    certain values of n to not be reported: the loop stops as soon as a
    Sample object is true, which may not necessarily be the lowest n
    depending on processing ordering. Stopping at that point means missing
    out on the output of the slower children.

--- chapters/06/samplesizeCI2.py
+++ chapters/06/samplesizeCI2.py
@@ -4,7 +4,6 @@ import math
 import logging
 import itertools as it
 import functools as ft
-import multiprocessing as mp
 from argparse import ArgumentParser
 
 import scipy.stats as st
@@ -53,29 +52,28 @@ arguments.add_argument('--delta', type=float,
 arguments.add_argument('--sigma', type=float,
                        help='Variance estimate for score differences')
 arguments.add_argument('--use-gamma', action='store_true')
-arguments.add_argument('--workers', type=int, default=mp.cpu_count())
 args = arguments.parse_args()
 
-with mp.Pool(args.workers) as pool:
-    zalpha = st.norm.ppf(1 - args.alpha / 2)
-    n = 4 * zalpha ** 2 * args.sigma / args.delta ** 2
+S = WithGamma if args.use_gamma else WithoutGamma
+zalpha = st.norm.ppf(1 - args.alpha / 2)
+n = 4 * zalpha ** 2 * args.sigma / args.delta ** 2
 
-    logging.debug(n)
+logging.debug(n)
 
-    fieldnames = ('n', 'adequate', 'left', 'right')
-    writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
-    writer.writeheader()
+fieldnames = ('n', 'adequate', 'left', 'right')
+writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+writer.writeheader()
 
-    S = WithGamma if args.use_gamma else WithoutGamma
-    f = ft.partial(S, alpha=args.alpha, delta=args.delta, sigma=args.sigma)
+for i in it.count(math.floor(n)):
+    s = S(i, args.alpha, args.delta, args.sigma)
 
-    for s in pool.imap_unordered(f, it.count(math.floor(n))):
-        found = bool(s)
-        row = [
-            int(s),
-            found,
-            *list(s),
-        ]
-        writer.writerow(dict(zip(fieldnames, row)))
-        if found:
-            break
+    found = bool(s)
+    row = [
+        int(s),
+        found,
+        *list(s),
+    ]
+    writer.writerow(dict(zip(fieldnames, row)))
+
+    if found:
+        break
