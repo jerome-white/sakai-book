88bb574ad0fdc33e27285cff26e783257e452555
Jerome White <jerome@thewex.net>
2019-04-27 02:05:02 +0530

    "Topic set size design with one-way ANOVA" (Section 6.4)
    Set size design in ANOVA requires linear approximations of lambda's
    (Table 6.4) Those approximations (that table) is recreated in the
    added CSV file.

---
+++ chapters/06/samplesizeANOVA2.py
@@ -0,0 +1,100 @@
+import sys
+import csv
+import math
+import logging
+import itertools as it
+import scipy.stats as st
+import multiprocessing as mp
+from pathlib import Path
+from argparse import ArgumentParser
+
+import pandas as pd
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S')
+
+class Sample:
+    def __init__(self, n, alpha, beta, delta, m):
+        self.n = n
+        self.beta = beta
+
+        phiA = m - 1
+        phiE = m * (self.n - 1)
+        w = st.f.ppf(1 - alpha, phiA, phiE)
+        cA = (phiA + 2 * self.n * delta) / (phiA + self.n * delta)
+        phiA_ = (phiA + self.n * delta) ** 2 / (phiA + 2 * self.n * delta)
+        self.u = ((math.sqrt(w / phiE) *
+                   math.sqrt(2 * phiE - 1) -
+                   math.sqrt(cA / phiA) *
+                   math.sqrt(2 * phiA_ - 1)) /
+                  math.sqrt(cA / phiA - w / phiE))
+
+    def __float__(self):
+        return float(1 - st.norm.cdf(self.u))
+
+    def __int__(self):
+        return self.n
+
+    def __bool__(self):
+        return 1 - self.beta < float(self)
+
+class Parameters(dict):
+    def __str__(self):
+        return ' '.join([ x + ': ' + str(y) for (x, y) in self.items() ])
+
+def func(incoming, outgoing, args):
+    while True:
+        params = incoming.get()
+
+        lamb = params['x1'] + params['x2'] * math.sqrt(args.systems - 1)
+        delta = args.difference ** 2 / (2 * args.sigma)
+        n = lamb / delta
+
+        logging.debug('n: {} {}'.format(n, params))
+
+        s = True
+        for i in it.takewhile(lambda _: s, range(math.floor(n), 0, -1)):
+            s = Sample(i, params['alpha'], params['beta'], delta, args.systems)
+            outgoing.put((params, s))
+        outgoing.put(None)
+
+arguments = ArgumentParser()
+arguments.add_argument('--difference', type=float,
+                       help='Minimum detectable difference')
+arguments.add_argument('--sigma', type=float,
+                       help='Variance estimate for score differences')
+arguments.add_argument('--systems', type=int,
+                       help='Number of systems compared')
+arguments.add_argument('--approximations', type=Path,
+                       help='Table 6.4')
+arguments.add_argument('--workers', type=int, default=mp.cpu_count())
+args = arguments.parse_args()
+
+incoming = mp.Queue()
+outgoing = mp.Queue()
+
+with mp.Pool(args.workers, func, (outgoing, incoming, args)):
+    df = pd.read_csv(args.approximations, index_col=None)
+    for i in df.itertuples(index=False):
+        outgoing.put(Parameters(i._asdict()))
+
+    keys = ('n', 'adequate', 'achieved')
+    writer = None
+    jobs = len(df)
+
+    while jobs:
+        result = incoming.get()
+        if result is None:
+            jobs -= 1
+        else:
+            (params, sample) = result
+
+            values = [ f(sample) for f in (int, bool, float) ]
+            row = { x: params[x] for x in ('alpha', 'beta') }
+            row.update(dict(zip(keys, values)))
+
+            if writer is None:
+                writer = csv.DictWriter(sys.stdout, fieldnames=row.keys())
+                writer.writeheader()
+            writer.writerow(row)
