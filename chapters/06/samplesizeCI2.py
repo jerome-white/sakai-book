1a3ab031639e6b5453a5f49c8b1e805574c30769
Jerome White <jerome@thewex.net>
2019-04-27 05:17:25 +0530

    Support for gamma

--- chapters/06/samplesizeCI2.py
+++ chapters/06/samplesizeCI2.py
@@ -22,11 +22,8 @@ logging.basicConfig(level=logging.DEBUG,
 class Sample:
     def __init__(self, n, alpha, delta, sigma):
         self.n = n
-        phi = self.n - 1
-        t = st.t.ppf(1 - alpha / 2, phi)
-
-        self.left = delta / (2 * math.sqrt(sigma))
-        self.right = t * (1 - 1 / (4 * phi)) / math.sqrt(self.n)
+        self.phi = self.n - 1
+        self.t = st.t.ppf(1 - alpha / 2, self.phi)
 
     def __iter__(self):
         yield from (self.left, self.right)
@@ -37,6 +34,23 @@ class Sample:
     def __bool__(self):
         return bool(self.right <= self.left)
 
+class WithoutGamma(Sample):
+    def __init__(self, n, alpha, delta, sigma):
+        super().__init__(n, alpha, delta, sigma)
+
+        self.left = delta / (2 * math.sqrt(sigma))
+        self.right = self.t * (1 - 1 / (4 * self.phi)) / math.sqrt(self.n)
+
+class WithGamma(Sample):
+    def __init__(self, n, alpha, delta, sigma):
+        super().__init__(n, alpha, delta, sigma)
+
+        g1 = math.gamma((self.phi + 1) / 2)
+        g2 = math.gamma(self.phi / 2)
+
+        self.left = delta / (2 * math.sqrt(2) * math.sqrt(sigma))
+        self.right = self.t * g1 / (math.sqrt(self.n * self.phi) * g2)
+
 arguments = ArgumentParser()
 arguments.add_argument('--alpha', type=float,
                        help='Type I Error probability')
@@ -44,7 +58,7 @@ arguments.add_argument('--delta', type=float,
                        help='Cap on the expected width of the CI')
 arguments.add_argument('--sigma', type=float,
                        help='Variance estimate for score differences')
-arguments.add_argument('--radius', type=int, default=0)
+arguments.add_argument('--use-gamma', action='store_true')
 arguments.add_argument('--workers', type=int, default=mp.cpu_count())
 args = arguments.parse_args()
 
@@ -63,10 +77,9 @@ with mp.Pool(args.workers) as pool:
     writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
     writer.writeheader()
 
-    f = ft.partial(Sample,
-                   alpha=args.alpha,
-                   delta=args.delta,
-                   sigma=args.sigma)
+    S = WithGamma if args.use_gamma else WithoutGamma
+    f = ft.partial(S, alpha=args.alpha, delta=args.delta, sigma=args.sigma)
+
     for s in pool.imap_unordered(f, it.count(math.floor(n))):
         found = bool(s)
         row = [
