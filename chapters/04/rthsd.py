f04c8d8d2de212cb343d7b5e10a64ef013dd467e
Jerome White <jerome@thewex.net>
2020-09-09 21:00:02 +0530

    Allow user to specify baseline system

--- chapters/04/rthsd.py
+++ chapters/04/rthsd.py
@@ -5,16 +5,18 @@ from argparse import ArgumentParser
 from irstats import Scores, RandomisedTukey
 
 arguments = ArgumentParser()
-arguments.add_argument('--B', type=int, default=0)
+arguments.add_argument('--baseline')
+arguments.add_argument('--B', type=int, default=1)
 arguments.add_argument('--workers', type=int)
 args = arguments.parse_args()
 
 assert(args.B > 0)
 
-scores = Scores.from_csv(sys.stdin)
 writer = None
+scores = Scores.from_csv(sys.stdin)
+rthsd = RandomisedTukey(scores, args.B, args.workers, args.baseline)
 
-for i in RandomisedTukey(scores, args.B, args.workers):
+for i in rthsd:
     if writer is None:
         writer = csv.DictWriter(sys.stdout, fieldnames=i._fields)
         writer.writeheader()
