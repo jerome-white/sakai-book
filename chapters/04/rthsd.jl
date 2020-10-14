8f313d2e6bd59ee9af40e6f7dac776085abcf014
Jerome White <jerome@thewex.net>
2020-10-14 15:48:55 +0000

    Remove reliance on locks
    To tally larger than expected means, use a separate dimension for each
    thread so that they do not compete for access.

--- chapters/04/rthsd.jl
+++ chapters/04/rthsd.jl
@@ -4,7 +4,6 @@ using
     ArgParse,
     Statistics,
     DataFrames,
-    SharedArrays,
     Base.Threads,
     Combinatorics
 
@@ -21,6 +20,7 @@ function cliargs()
 end
 
 args = cliargs()
+workers = nthreads()
 
 #
 # data aquisition
@@ -38,22 +38,22 @@ raw = Matrix{Float64}(df)
 sysmeans = Matrix{Float64}(undef, nsystems, nsystems)
 for (i, j) in combinations(collect(enumerate(mean(raw; dims=1))), 2)
     for (m, n) in (i, j)
-        sysmeans[m, m] = n
+        sysmeans[m,m] = n
     end
     ((a, x), (b, y)) = (i, j)
-    sysmeans[a, b] = sysmeans[b, a] = abs(x - y)
+    sysmeans[a,b] = sysmeans[b,a] = abs(x - y)
 end
-avgs = Matrix{Float64}(undef, nthreads(), nsystems)
+avgs = Matrix{Float64}(undef, workers, nsystems)
 
 #
 # initialize counts
 #
-counts = SharedMatrix{Int}((nsystems, nsystems); init=0)
+counts = zeros(Int, nsystems, nsystems, workers)
 
 #
 # create views of the data
 #
-data  = Array{Float64}(undef, nsystems, nrow(df), nthreads())
+data  = Array{Float64}(undef, nsystems, nrow(df), workers)
 data .= transpose(raw)
 
 #
@@ -72,9 +72,9 @@ data .= transpose(raw)
 
     m = mean(ptr; dims=2)
     d = reduce(-, map(x -> x(m), (maximum, minimum)))
-    for (j, k) in shuffle.(combinations(1:nsystems, 2))
-        if d >= sysmeans[j, k]
-            counts[j, k] += 1
+    for (j, k) in combinations(1:nsystems, 2)
+        if d >= sysmeans[j,k]
+            counts[j,k,this] += 1
         end
     end
 end
@@ -93,8 +93,8 @@ cnames = [
 results = Matrix{Any}(undef, binomial(nsystems, 2), length(cnames))
 sysnames = names(df)
 for (i, (m, n)) in enumerate(combinations(1:nsystems, 2))
-    pvalue = (counts[m, n] + counts[n, m]) / args["B"]
-    (lhs, rhs) = mean.(eachcol(df[!, [m, n]]))
+    pvalue = sum(counts[m,n,:] + counts[n,m,:]) / args["B"]
+    (lhs, rhs) = mean.(eachcol(df[!, [m,n]]))
     results[i,:] = [
         sysnames[m],
         sysnames[n],
